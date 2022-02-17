daa5ed1d7e66cf1d62e8a0c70937f5bde0d5c5cb
Letícia Porres Lang <81568847+leticiaplang@users.noreply.github.com>
2022-02-17 11:27:53 -0300

    Update model_tree.py

--- model_tree.py
+++ model_tree.py
@@ -5,7 +5,7 @@ import pandas as pd  # fazer feature engineering manual
 class LinearRegression:
 
     def __init__(self):  # self referencia a classe
-        self.model_pipe = pk.load(open("/Users/lpl/Downloads/jupyter_doc/model_pipe.pkl", 'rb'))
+        self.model_pipe = pk.load(open("./model_pipe.pkl", 'rb'))
 
     def predict(self, df):
         return self.model_pipe.predict(df)
